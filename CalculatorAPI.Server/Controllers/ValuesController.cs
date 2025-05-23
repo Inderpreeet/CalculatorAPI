2db80c5082081859a9052af08cd601e00a965602
Inderpreeet <A00291274@mycambrian.ca>
2025-05-22 21:14:07 -0400

    Add project files.

---
+++ CalculatorAPI.Server/Controllers/ValuesController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CalculatorApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CalculatorController : ControllerBase
+    {
+        [HttpGet("add")]
+        public IActionResult Add(double x, double y) => Ok(x + y);
+
+        [HttpGet("subtract")]
+        public IActionResult Subtract(double x, double y) => Ok(x - y);
+
+        [HttpGet("multiply")]
+        public IActionResult Multiply(double x, double y) => Ok(x * y);
+
+        [HttpGet("divide")]
+        public IActionResult Divide(double x, double y)
+        {
+            if (y == 0) return BadRequest("Cannot divide by zero.");
+            return Ok(x / y);
+        }
+    }
+}
+
+namespace ReactApp1.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+    }
+}
+
+namespace CalculatorAPI.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+    }
+}
